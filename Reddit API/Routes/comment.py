f1a89e9c2432e43b14b2c1475a7f123f80515892
M.Rafay <pixelmultishot@gmail.com>
2026-03-08 17:34:34 +0500

    fix

--- Reddit API/Routes/comment.py
+++ Reddit API/Routes/comment.py
@@ -1,13 +1,25 @@
 from fastapi import APIRouter, status, HTTPException, Depends
 from sqlalchemy.orm import Session
-from Engines import schemas, models, database, oauth2
+from Engines import schemas, models, database, oauth2, rate_limiter
 
 router= APIRouter(tags=["Comment"])
 get_db= database.get_db
 get_current_user= oauth2.get_current_user
+rate_limit_user= rate_limiter.token_bucket_rate_limit
 
 @router.post("/post/{post_id}/comments") #id is post id
 def create_comment(post_id: int, request: schemas.Comment, db: Session= Depends(get_db), current_user: models.Users= Depends(get_current_user)):
+
+    key = f"tokenbucket:create_comment:user:{current_user.id}:post:{post_id}"
+
+    allowed = rate_limit_user(key=key, capacity=1, refill_rate=0.2)
+
+    if not allowed:
+        raise HTTPException(
+            status_code=429,
+            detail="Too many posts. Slow down."
+        )
+
     comment= models.Comments(content=request.content, user_id= current_user.id, post_id=post_id, parent_id= request.parent_id or None)
     db.add(comment)
     db.commit()
